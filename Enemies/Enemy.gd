e195b96c9b3c62c148112ce401bdb6f724e799d2
Benjamin Wong <chiyeonwastaken@gmail.com>
2021-10-23 15:39:54 -0700

    enemy damage & death

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -10,3 +10,11 @@ export var health : int = 10
 export var damage : int = 1;
 export var attack_speed : float = 1.5;
 export var move_speed : float = 2;
+
+func Damage(amount):
+	health -= amount;
+	if(health <= 0):
+		Die();
+
+func Die():
+	self.queue_free();
