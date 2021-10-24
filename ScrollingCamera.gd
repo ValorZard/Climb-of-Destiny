8e2830e1264fd9e1cf72a069d54b8576cd172979
Shane Parslow <shaneparslow808@gmail.com>
2021-10-24 16:48:15 -0700

    fix camera offset

--- ScrollingCamera.gd
+++ ScrollingCamera.gd
@@ -3,4 +3,4 @@ extends Camera2D
 onready var player = get_node("../Player")
 
 func _process(delta):
-	self.position.y = player.get_global_position().y - 300
+	self.position.y = player.get_global_position().y - 150
