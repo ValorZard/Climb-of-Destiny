c5a2285244fd0ff88a42fbc743191f0a2b0467a4
Shane Parslow <shaneparslow808@gmail.com>
2021-10-24 16:19:42 -0700

    camera fixes

--- ScrollingCamera.gd
+++ ScrollingCamera.gd
@@ -1,8 +1,6 @@
 extends Camera2D
 
-onready var player = get_tree().current_scene.get_node("Player")
+onready var player = get_node("../Player")
 
 func _process(delta):
-	position.y = player.get_global_position().y - 300
-	position.x = 0
-
+	self.position.y = player.get_global_position().y - 300
