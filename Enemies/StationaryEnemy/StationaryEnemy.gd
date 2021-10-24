bd3c4f3c46bfa654925c207227908e2d8240fd6d
Benjamin Wong <chiyeonwastaken@gmail.com>
2021-10-23 18:13:41 -0700

    fixed animations for stationary enemy

--- Enemies/StationaryEnemy/StationaryEnemy.gd
+++ Enemies/StationaryEnemy/StationaryEnemy.gd
@@ -10,11 +10,13 @@ onready var AnimationPlayer = $AnimationPlayer;
 # start attack anim when player enters radius
 func _on_AttackRadius_body_entered(body):
 	if body is Player:
-		if(!AnimationPlayer.is_playing()):
-			AnimationPlayer.play("Attack");
+		AnimationPlayer.play("Attack");
 
 # damage player if they collide with head hitbox
 # hitbox is enabled/disabled in the Attack animation 
 func _on_HeadHitbox_body_entered(body):
 	if body is Player:
 		print("Player hit! but player health isn't set up yet. TODO");
+
+func play_idle_animation():
+	AnimationPlayer.play("Idle");
