ff08e0f7ee4e005d8093eeac483435370c6cb0de
Shane Parslow <shaneparslow808@gmail.com>
2021-10-23 17:16:04 -0700

    completely redo player, new BulletSpawner, bullet changes

--- Bullets/Bullet.gd
+++ Bullets/Bullet.gd
@@ -1,44 +1,31 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 class_name Bullet
 
-var velocity := Vector2.ZERO
-var default_velocity := Vector2(800, 0)
+var bullet_velocity := Vector2.ZERO
 var damage : int = 10
-var facing_right := true
 
-var node_shot_from : Node # the owner of this node
+#var node_shot_from : Node # the owner of this node
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	add_to_group("Bullets")
-	velocity = default_velocity
-
-func destroy_self():
-	self.queue_free()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	self.position += velocity * delta
-	# set direction of velocity of bullet of the direction player is facing
-	if !facing_right:
-		if velocity.x > 0:
-			velocity.x = -velocity.x
-	else:
-		if velocity.x < 0:
-			velocity.x = -velocity.x
-	#print($Timer.is_stopped())
-	#print($Timer.time_left)
-	# when timer is done, delete bullet so we dont have a bunch of random bullets exists
-	if $Timer.is_stopped():
-		destroy_self()
+func _on_DespawnTimer_timeout():
+	destroy_self()
 
 func _on_Bullet_body_entered(body):
 	if body is Enemy:
 		body.health -= 1
 	if !body.is_in_group("Bullets"):
 		destroy_self()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("Bullets")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	self.position += bullet_velocity * delta
+
+func destroy_self():
+	self.queue_free()
+
+func set_velocity(vel: Vector2):
+	bullet_velocity = vel
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,101 +1,101 @@
-# part of code taken from KidsCanCode 
-# https://kidscancode.org/godot_recipes/2d/platform_character/
-
 extends KinematicBody2D
 
-class_name Player
-
-export var speed : float = 600
+export var walk_speed : float = 600
+export var walk_acceleration : float = 0.25
 export var jump_speed : float = -1000
 export var gravity : float = 70
 export var friction : float = 0.1
-export var acceleration : float = 0.25
 
-export var max_air_jumps : int = 1
-var air_jumps : int = max_air_jumps
-var is_jumping : bool = false
+export var max_air_jumps : int = 3
 
-var velocity : Vector2 = Vector2.ZERO
-var direction : int = 0
-var facing_right := true
+var velocity: Vector2 = Vector2.ZERO
+var air_jumps: int = max_air_jumps
+var can_shoot: bool = true
 
-var bullet_prefab : PackedScene = preload("res://Bullets/Bullet.tscn")
+var bullet_prefab = preload("res://Bullets/Bullet.tscn")
 
-func get_input():
-	direction = 0
+onready var bullet_timer: Timer = get_node("BulletTimer")
+onready var bullet_node: Node = get_tree().get_current_scene().get_node("Bullets")
+# All bullet spawners that are children of this node
+# How do you type this?
+onready var bullet_spawners = get_child_bullet_spawners()
+
+
+func _on_BulletTimer_timeout():
+	can_shoot = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	shoot()
+	move()
+
+# Calls each function to manipulate velocity
+func move():
+	move_horizontal()
+	move_gravity()
+	move_jump()
+	# Add other movement abilities here
+	
+	# Commit velocities and handle collisions
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+# Get all BulletSpawners that are children of this node
+func get_child_bullet_spawners():
+	var spawners = []
+	for child in get_children():
+		if child is BulletSpawner:
+			spawners.append(child)
+	return spawners
+
+# Get BulletSpawner that is closest to cursor
+# Add ability to use arrow keys?
+func get_closest_bullet_spawner():
+	var i_distance: float = INF
+	var i_closest: BulletSpawner
+	var mouse_pos = get_viewport().get_mouse_position()
+	for spawner in bullet_spawners:
+		var cur_distance = spawner.get_global_position().distance_to(mouse_pos)
+		if cur_distance < i_distance:
+			i_distance = cur_distance
+			i_closest = spawner
+	return i_closest
+
+# Instantiate and set position of bullets each time timer fires
+func shoot():
+	if Input.is_action_pressed("shoot") and can_shoot:
+		get_closest_bullet_spawner().shoot()
+		can_shoot = false
+		bullet_timer.start(0)
+
+# Walking/air strafe and friction if not walking
+func move_horizontal():
+	# Can if function into 'if' if different case needed for air
+	# Ex: move_horizontal_ground and move_horizontal_air
+	var direction = get_walk_direction()
+	if direction:
+		velocity.x = lerp(velocity.x, direction*walk_speed, walk_acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+
+func get_walk_direction():
+	var direction : int = 0
+	
 	if Input.is_action_pressed("walk_right"):
 		direction += 1
 	if Input.is_action_pressed("walk_left"):
 		direction -= 1
-	
-	# check if the facing direction is same as mouse
-	facing_right = get_viewport().get_mouse_position().x - self.position.x >= 0
-		
+	return direction
 
-func set_horizontal_movement():
-	if direction != 0:
-		velocity.x = lerp(velocity.x, direction * speed, acceleration)
-	else:
-		velocity.x = lerp(velocity.x, 0, friction)
+func move_gravity():
+	velocity.y += gravity
 
-func set_vertical_movement(delta):
-	# when on floor and jump is not buffered, there shouldn't be any y velocity
-	# if jump is buffered, y velocity should equal jump speed
-	# if is in air, the player should be affected by gravity
-	if is_on_floor() and !is_jumping:
-		velocity.y = 0
-		# refill air jumps now that we've landed
+func move_jump():
+	if is_on_floor():
 		air_jumps = max_air_jumps
-	elif is_on_floor():
-		is_jumping = false
-	else:
-		velocity.y += gravity
-
-func set_movement(delta):
-	set_horizontal_movement()
-	set_vertical_movement(delta)
-	do_jump()
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-
-func do_jump():
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
-		elif air_jumps > 0:
-			velocity.y = jump_speed
-			air_jumps -= 1
-
-func set_bullet_exit():
-	# flip position of bullet exit based on facing direction
-	if facing_right and $BulletExit.position.x < 0:
-		$BulletExit.position.x = -$BulletExit.position.x
-	elif !facing_right and $BulletExit.position.x > 0:
-		$BulletExit.position.x = -$BulletExit.position.x
-
-func check_shoot():
-	if Input.is_action_pressed("shoot"):
-		# if the timer between shots is over
-		if $BulletTimer.is_stopped():
-			# spawn bullet
-			var bullet := bullet_prefab.instance()
-			get_tree().current_scene.get_node("Bullets").add_child(bullet)
-			# set bullet information
-			bullet.position = $BulletExit.global_position
-			bullet.facing_right = facing_right
-			bullet.node_shot_from = self
-			# start timer again
-			$BulletTimer.start()
-			
-
-func _physics_process(delta):
-	get_input()
-	set_movement(delta)
-	set_bullet_exit()
-	check_shoot()
-	print_player()
-
-func print_player():
-	var print_string := ""
-	print_string += "Direction: " + str(direction) + "\n"
-	$Label.text = print_string
+	if Input.is_action_just_pressed("jump") and air_jumps:
+		velocity.y = jump_speed
+		air_jumps -= 1
